9468269e52ea2827117a83d80820216423ad5597
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2022-07-14 19:12:14 +0200

    still writing member function 4 tests failing

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -150,6 +150,31 @@ void Controller::handleNewFood(const FoodResp& requestedFood)
     m_foodPosition = std::make_pair(requestedFood.x, requestedFood.y);
 }
 
+void Controller::handlePauseEvent(const PauseInd& PauseVar)
+{
+    
+    EventT<PauseInd> pauseEvent;
+
+    if (pauseEvent)
+        PauseInd = 0;
+    else
+        PauseInd = 1
+
+
+ // auto time = PauseInd.MESSAGE_ID;
+
+
+
+
+
+
+
+
+
+}   
+
+
+
 bool Controller::doesCollideWithSnake(const Controller::Segment &newSegment) const
 {
     for (auto segment : m_segments) {
@@ -221,8 +246,16 @@ void Controller::receive(std::unique_ptr<Event> e)
         case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+        case PauseInd::MESSAGE_ID: return  handlePauseEvent(*static_cast<EventT<PauseInd> const&>(*e));
         default: throw UnexpectedEventException();
     };
+
+    
+
+
+    
+
+
 }
 
 } // namespace Snake
