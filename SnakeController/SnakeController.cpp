f392c123bec570d2a49a860964d9bc446a9f21b5
matkad92 <mateuszkadziolka92@gmail.com>
2022-07-15 09:13:50 +0200

    handlePauseInd added

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -19,7 +19,9 @@ UnexpectedEventException::UnexpectedEventException()
 Controller::Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePort, std::string const& p_config)
     : m_displayPort(p_displayPort),
       m_foodPort(p_foodPort),
-      m_scorePort(p_scorePort)
+      m_scorePort(p_scorePort),
+      isPaused(false)
+
 {
     std::istringstream istr(p_config);
     char w, f, s, d;
@@ -82,6 +84,7 @@ void Controller::handleTimePassed(const TimeoutInd&)
         notifyAboutFailure();
         return;
     }
+    
     else
     {
         for (auto &segment : m_segments) {
@@ -104,6 +107,11 @@ void Controller::handleDirectionChange(const DirectionInd& directionInd)
     if ((m_currentDirection & 0b01) != (direction & 0b01)) {
         m_currentDirection = direction;
     }
+}
+
+void Controller::handlePauseInd(const PauseInd&)
+{
+    isPaused = not isPaused;
 }
 
 void Controller::handleFoodPositionChange(const FoodInd& receivedFood)
@@ -221,6 +229,7 @@ void Controller::receive(std::unique_ptr<Event> e)
         case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+        case PauseInd::MESSAGE_ID: return handlePauseInd(*static_cast<EventT<PauseInd> const&>(*e));
         default: throw UnexpectedEventException();
     };
 }
